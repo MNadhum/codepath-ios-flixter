1cff8e4a7d9d0a8efc8a642d3662713b1fa00664
Marven <57809115+MNadhum@users.noreply.github.com>
2020-10-03 17:02:09 -0400

    Unit2 completed commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Marven Nadhum on 10/3/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
